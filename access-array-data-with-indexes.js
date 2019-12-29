0213854518d6ca22a1422e9a7eec1fb2743698b4
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2019-12-29 12:10:57 -0600

    Code review

--- access-array-data-with-indexes.js
+++ access-array-data-with-indexes.js
@@ -1,5 +1,7 @@
 //Create a variable called myData and set it to equal the first value of myArray using bracket notation.
 
+// this needs changing
+
 var myArray = [50,60,70];
 
 // Only change code below this line.
