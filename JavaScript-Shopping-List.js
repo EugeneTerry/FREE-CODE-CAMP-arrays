dc7b5aa6ee17b152db95e2929d5212d7a22cccfb
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2019-12-29 21:25:04 -0600

    Create JavaScript-Shopping-List.js

---
+++ JavaScript-Shopping-List.js
@@ -0,0 +1,7 @@
+//Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.
+
+var myList = [["Ice Cream", 3],["Cake",4],["Rice",5],["Candy",18],["Juice", 9]];
+console.log(myList)
