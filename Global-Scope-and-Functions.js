8c55bd07351863f7b9af8acd4c345ba5e826c1e7
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2019-12-30 20:19:51 -0600

    Moved Comment Up

--- Global-Scope-and-Functions.js
+++ Global-Scope-and-Functions.js
@@ -4,10 +4,9 @@ Inside function fun1, assign 5 to oopsGlobal without using the var keyword.
 // Declare your variable here
 var myGlobal=10
 
+// Assign 5 to oopsGlobal Here
 function fun1() {
   oopsGlobal=5
-  // Assign 5 to oopsGlobal Here
-
 }
 
 // Only change code above this line
