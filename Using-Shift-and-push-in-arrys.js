322a2d5683aee6af8bf4523b85958d58fc926c46
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2019-12-30 21:43:42 -0600

    Create Using-Shift-and-push-in-arrys.js

---
+++ Using-Shift-and-push-in-arrys.js
@@ -0,0 +1,16 @@
+//Write a function nextInLine which takes an array (arr) and a number (item) as arguments.
+Add the number to the end of the array, then remove the first element of the array.
+The nextInLine function should then return the element that was removed.
+
+function nextInLine(arr, item) {
+  // I had to go back to my old lessons and find shift/push methods.
+  return arr.shift();  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
