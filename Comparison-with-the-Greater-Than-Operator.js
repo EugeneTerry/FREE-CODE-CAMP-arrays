63310e3f1d050759b52992abb705b7d9dad6ddf7
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-04 18:47:33 -0600

    Create Comparison-with-the-Greater-Than-Operator.js

---
+++ Comparison-with-the-Greater-Than-Operator.js
@@ -0,0 +1,19 @@
+//Comparison with the Greater Than Operator
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    console.log ("Over 100")
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    console.log ("Over 10")
+    return "Over 10";
+  }
+  console.log ("10 or under")
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(101);
