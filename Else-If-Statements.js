a60d40f67edb18842b1dca9aa65c073adf494f7e
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-04 21:35:28 -0600

    No errors this time

--- Else-If-Statements.js
+++ Else-If-Statements.js
@@ -1,14 +1,14 @@
 function testElseIf(val) {
   if (val > 10) {
-    console.log ("Greater than 10")
+    console.log("Greater than 10")
     return "Greater than 10";
   } else if (val < 5) {
-    console.log ("Smaller than 5")
-    return "Smaller than 5";
-  } else 
-  console.log ("Between 5 and 10")
-  return "Between 5 and 10";
+    console.log("Smaller than 5")
+    return "Smaller than 5"; 
+  } else {
+    console.log("Between 5 and 10")
+    return "Between 5 and 10";
+    }
 }
-
 // Change this value to test
-testElseIf(60);
+testElseIf(6);
