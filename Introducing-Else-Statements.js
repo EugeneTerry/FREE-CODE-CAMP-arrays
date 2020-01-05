fbbde2c5e12ec702d5bacd2626c8c57bc9546282
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-04 20:45:43 -0600

    Create Introducing-Else-Statements.js

---
+++ Introducing-Else-Statements.js
@@ -0,0 +1,21 @@
+//Combine the if statements into a single if/else statement.
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    console.log ("Bigger than 5")
+    result = "Bigger than 5";
+  }
+else {
+    console.log ("5 or Smaller")
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(8);
