96702a382e309897e812a0aa0cc8902b652cfd31
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-13 23:35:10 -0600

    Create Selecting from Many Options with Switch Statements.js

---
+++ Selecting from Many Options with Switch Statements.js
@@ -0,0 +1,35 @@
+//Write a switch statement which tests val and sets answer for the following conditions:
+//1 - "alpha"
+//2 - "beta"
+//3 - "gamma"
+//4 - "delta"
+
+
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+
+switch(val) {
+  case 1:
+    answer = "alpha";
+    console.log("alpha");
+    break;
+  case 2:
+    answer = "beta";
+    console.log("beta");
+    break;
+  case 3:
+    answer = "gamma";
+    console.log("gamma");
+    break;
+  case 4:
+    answer = "delta";
+    console.log("delta");
+    break;
+ }
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+caseInSwitch(2);
