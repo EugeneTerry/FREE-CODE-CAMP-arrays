7ddb44358fbb67fa4b7ef742a2fe4647a13ffd8b
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-16 23:05:43 -0600

    Added scorecard instructions

--- golf-scoring/main2.js
+++ golf-scoring/main2.js
@@ -23,10 +23,16 @@ function golfScore(par, strokes) {
     document.getElementById("myText").innerHTML = "It's time to go home now";
   }
 }
+
 btn.addEventListener("click", (e)=> {
   e.preventDefault();
   var par = parseInt(document.getElementById("par").value);
   var stroke = parseInt(document.getElementById("stroke").value);  
 
   golfScore(par,stroke);
+  //This adds the variable (myScore) to the dom's output in a sentance. 
+  var pi = "Please add a " ;
+  var ei = " to your score card.";
+  var myScore = stroke - par;
+  document.getElementById("myBook").innerHTML = pi + myScore + ei;
 })
