4d967abf67eaeb292ad93d4211ddc5bc8e26aaa6
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-28 19:04:00 -0600

    Create callObjectValues.js

---
+++ callObjectValues.js
@@ -0,0 +1,13 @@
+//With this I can call specific properties of an object
+
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;      
+var shirtValue = testObj.shirt;
+
+ console.log(hatValue) //this should output whatewver property the hat has which is "ballcap"
+ console.log(shirtValue) //this should output whatewver property the shirt has which is "jersey"
