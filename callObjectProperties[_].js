b03bf331078b014f717e3c39a552daaca0cbc8f7
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-28 20:10:12 -0600

    Create callObjectProperties[_].js

---
+++ callObjectProperties[_].js
@@ -0,0 +1,12 @@
+// this allowed me to call a property of an object using brackets
+
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+
+var entreeValue = testObj["an entree"];  //this allowed me to assign the call to a variable 
+var drinkValue = testObj["the drink"];  
+console.log(entreeValue) // lets me see it happen
