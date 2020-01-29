adc5627920d7195f155065afb35e7701ec077ea6
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-01-28 18:50:47 -0600

    Create simpleObject.js

---
+++ simpleObject.js
@@ -0,0 +1,10 @@
+var myDog = {
+  "name": "Rex",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Man", "Bone"]
+
+};
+
+// "name" is a string, "legs"
+//and "tails" are numbers, and "friends" is an array.
