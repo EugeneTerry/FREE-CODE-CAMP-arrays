11ffc25f93d711e10566321d2b958eafdd3c5615
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-02-01 11:23:33 -0600

    Added the delete option at the bottom

--- Add-Delete-New-Properties-to-a-JavaScript-Object.js
+++ Add-Delete-New-Properties-to-a-JavaScript-Object.js
@@ -7,5 +7,8 @@ var myDog = {
   "friends": ["freeCodeCamp Campers"]
 };
 //Here I added to the object by using the dot or brackets
-myDog.bark = "woof"
-myDog["bark"] = "woof"
+myDog.bark = "woof";
+myDog["bark"] = "woof";
+//Here I deleted from the object by using the dot or brackets
+delete myDog["tails"];
+delete myDog.tails;
