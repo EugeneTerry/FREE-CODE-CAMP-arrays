cc83d78885351d42939a447cdebfca66ef0fd2c8
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-02-01 11:19:11 -0600

    Create Add-New-Properties-to-a-JavaScript-Object.js

---
+++ Add-New-Properties-to-a-JavaScript-Object.js
@@ -0,0 +1,11 @@
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+//Here I added to the object by using the dot or brackets
+myDog.bark = "woof"
+myDog["bark"] = "woof"
