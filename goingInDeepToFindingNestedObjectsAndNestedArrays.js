7fc6050c46e690aa160a18ce41ee2529169d06fd
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-02-06 15:24:27 -0600

    Create goingInDeepToFindingNestedObjectsAndNestedArrays.js

---
+++ goingInDeepToFindingNestedObjectsAndNestedArrays.js
@@ -0,0 +1,22 @@
+// Here I am looking for objects within nested arrays
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+var secondTree = myPlants[1].list[1]; // I used the numbers to locate the second tree object in the nested array
+console.log(secondTree)
