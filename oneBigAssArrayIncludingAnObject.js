78d2777f7f5b988f1ec2d908592ddc3e9e30d522
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-02-05 22:56:38 -0600

    Create oneBigAssArrayIncludingAnObject.js

---
+++ oneBigAssArrayIncludingAnObject.js
@@ -0,0 +1,28 @@
+// below you can see the starting bracket of the array
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  ,
+  // here is the second part of the array with another object in it
+  // you can see the seperation by the use of a comma
+  {
+    "artist": "LL Cool J",
+      "title": "I'm Bad",
+      "release_year": 1985,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ]
+      }
+  ]; // here you can see the end of the array braket
+  
