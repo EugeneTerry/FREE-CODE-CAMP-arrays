8c305b4ca8d7de7b886d371f80588703e1fed2c5
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-02-07 13:13:21 -0600

    Create forLoopsGoingByTwos.js

---
+++ Loops/forLoopsGoingByTwos.js
@@ -0,0 +1,7 @@
+//This creats an array with numbers going up by 2's
+//Depending what number you start off with will determine if the numbers will be odd or even
+var myArray = [];
+for (var i = 1; i <= 9; i+= 2)
+  myArray.push(i);
+  console.log(myArray)
+// this array should look like [1,3,5,7,9]
