691fde262d4b562605e0c2031b1ad773e070bced
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-02-17 23:23:21 -0600

    Create Nesting-For-Loops.js

---
+++ Loops/Nesting-For-Loops.js
@@ -0,0 +1,13 @@
+//Modify function multiplyAll so that it multiplies the product variable by each number in the sub-arrays of arr
+function multiplyAll(arr) {
+  var product = 1;
+for (var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+    product = product * arr[i][j];
+  }
+}
+console.log(product);
+  return product;
+}
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
