eb11524a5128dde01ddd340165f5399baa1abefc
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-11 00:34:57 -0500

    Create Creating_Random_Whole_numbers.js

---
+++ Math/Creating_Random_Whole_numbers.js
@@ -0,0 +1,7 @@
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+//Math.random() generates a random decimal
+//Math.floor() rounds the number down to its nearest whole number.
+  console.log(Math.floor(Math.random() * 10))
+  return Math.floor(Math.random() * 10);
