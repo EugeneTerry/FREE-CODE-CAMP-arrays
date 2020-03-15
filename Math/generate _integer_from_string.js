051f3190bccf079a7609adf46e1319d012c82abb
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-15 12:46:50 -0500

    Create generate _integer_from_string.js

---
+++ Math/generate _integer_from_string.js
@@ -0,0 +1,6 @@
+function convertToInteger(str) {
+console.log(parseInt(str)); //The parseInt() function parses a string and returns an integer
+return parseInt(str);
+}
+
+convertToInteger("001"); // this will return a "1" as an integer 
