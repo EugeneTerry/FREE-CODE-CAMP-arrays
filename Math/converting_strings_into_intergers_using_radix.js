8ab8bb7a588fd6141f65aa2c5e02eadd2b50c94c
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-15 13:00:14 -0500

    Create converting_strings_into_intergers_using_radix.js

---
+++ Math/converting_strings_into_intergers_using_radix.js
@@ -0,0 +1,10 @@
+// example: parseInt(string, radix);
+
+function convertToInteger(str) {
+    console.log (parseInt(str, 2))
+return parseInt(str, 2); 
+//The radix variable "2" says that "0110" is in the binary system, or base 2.
+//This example converts the string "0110" to an integer 6.
+}
+
+convertToInteger("0110"); 
