c66b2910045b1c73c73abe2413e14a6f3c6eb32d
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2020-03-15 15:13:47 -0500

    Create One_line_if_else_expression.js

---
+++ Math/One_line_if_else_expression.js
@@ -0,0 +1,8 @@
+//Change an if-else statement to a Conditional (Ternary) Operator or one line if-else expression
+
+function checkEqual(a, b) {
+    a == b ? console.log ("Equal") : console.log ("Not Equal");
+    return a == b ? "Equal" : "Not Equal"; //i could use any inequality = < >
+}
+
+checkEqual(1, 1);
