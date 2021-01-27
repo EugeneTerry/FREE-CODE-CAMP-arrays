01cdd7bbca433ed190b1db586fef42fd38b12430
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-01-26 21:29:19 -0600

    Create variables.js

---
+++ Intro Into Javascript/variables.js
@@ -0,0 +1,12 @@
+let gameLock = "true"
+let gameFree = "false"
+console.log(`My dog says that my ${gameFree} nature will create a ${gameLock} sense of security.`) // Prints out My dog says that my false nature will create a true sense of security.
+
+let changeMe = true
+changeMe = false // Since I used 'let' I can change the value of changeMe within the code.
+console.log(changeMe)
+
+const entree = 'Enchiladas'
+console.log(entree)
+//entree = 'Tacos'
+// you cant reassign a const
