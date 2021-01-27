2d992daf00d1903c99939e5fbe4ebbf098e426cd
Eugene Terry <59325156+EugeneTerry@users.noreply.github.com>
2021-01-26 20:51:56 -0600

    Create dotOperatorMethodStuff.js

---
+++ Intro Into Javascript/dotOperatorMethodStuff.js
@@ -0,0 +1,5 @@
+console.log(Math.random() * 100) // Math.random() generates numbers between 0 and 1
+console.log(Math.floor(Math.random() * 100)) //Math.floor() makes the random number rounds down to nearest interger.
+console.log(Math.ceil((Math.random() * 43.8) )) //returns the smallest integer greater than or equal to a decimal number.
+console.log(Number.isInteger(2017)) //Number.isInteger() checks if a number is an integer.
+console.log(Math.ceil(5.1));//Math.ceil() makes the random number rounds up to nearest interger.
